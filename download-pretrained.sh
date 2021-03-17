3e048a1df1067a2d97fc2d88957311dbb438b0f2
Jimmy Wu <jimmywu@alum.mit.edu>
2021-03-17 15:55:12 -0700

    Fix download URLs

--- download-pretrained.sh
+++ download-pretrained.sh
@@ -1,9 +1,9 @@
 # Downloads four pretrained policies, one for each test environment
-wget -c https://vision.princeton.edu/projects/2020/spatial-action-maps/pretrained/logs/20200125T213536-small_empty/config.yml -P logs/20200125T213536-small_empty
-wget -c https://vision.princeton.edu/projects/2020/spatial-action-maps/pretrained/checkpoints/20200125T213536-small_empty/model_00061000.pth.tar -P checkpoints/20200125T213536-small_empty
-wget -c https://vision.princeton.edu/projects/2020/spatial-action-maps/pretrained/logs/20200126T171505-small_columns/config.yml -P logs/20200126T171505-small_columns
-wget -c https://vision.princeton.edu/projects/2020/spatial-action-maps/pretrained/checkpoints/20200126T171505-small_columns/model_00061000.pth.tar -P checkpoints/20200126T171505-small_columns
-wget -c https://vision.princeton.edu/projects/2020/spatial-action-maps/pretrained/logs/20200126T165108-large_columns/config.yml -P logs/20200126T165108-large_columns
-wget -c https://vision.princeton.edu/projects/2020/spatial-action-maps/pretrained/checkpoints/20200126T165108-large_columns/model_00061000.pth.tar -P checkpoints/20200126T165108-large_columns
-wget -c https://vision.princeton.edu/projects/2020/spatial-action-maps/pretrained/logs/20200127T015539-large_divider/config.yml -P logs/20200127T015539-large_divider
-wget -c https://vision.princeton.edu/projects/2020/spatial-action-maps/pretrained/checkpoints/20200127T015539-large_divider/model_00061000.pth.tar -P checkpoints/20200127T015539-large_divider
+wget -c https://spatial-action-maps.cs.princeton.edu/pretrained/logs/20200125T213536-small_empty/config.yml -P logs/20200125T213536-small_empty
+wget -c https://spatial-action-maps.cs.princeton.edu/pretrained/checkpoints/20200125T213536-small_empty/model_00061000.pth.tar -P checkpoints/20200125T213536-small_empty
+wget -c https://spatial-action-maps.cs.princeton.edu/pretrained/logs/20200126T171505-small_columns/config.yml -P logs/20200126T171505-small_columns
+wget -c https://spatial-action-maps.cs.princeton.edu/pretrained/checkpoints/20200126T171505-small_columns/model_00061000.pth.tar -P checkpoints/20200126T171505-small_columns
+wget -c https://spatial-action-maps.cs.princeton.edu/pretrained/logs/20200126T165108-large_columns/config.yml -P logs/20200126T165108-large_columns
+wget -c https://spatial-action-maps.cs.princeton.edu/pretrained/checkpoints/20200126T165108-large_columns/model_00061000.pth.tar -P checkpoints/20200126T165108-large_columns
+wget -c https://spatial-action-maps.cs.princeton.edu/pretrained/logs/20200127T015539-large_divider/config.yml -P logs/20200127T015539-large_divider
+wget -c https://spatial-action-maps.cs.princeton.edu/pretrained/checkpoints/20200127T015539-large_divider/model_00061000.pth.tar -P checkpoints/20200127T015539-large_divider
